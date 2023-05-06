e8f57f24bc31dbe66ae8bed75d76bf68035bc36d
Tomat <xxlennygamerxx@gmail.com>
2023-05-06 01:48:17 -0700

    Finally, working detours.

--- isaac-inject/src/os/windows.rs
+++ isaac-inject/src/os/windows.rs
@@ -1,3 +1,4 @@
+use retour_utils::hook_module;
 use windows::{
     w,
     Win32::{
@@ -6,10 +7,22 @@ use windows::{
     },
 };
 
+#[hook_module("user32.dll")]
+mod user32 {
+    use windows::{core::PCWSTR, w, Win32::Foundation::HWND};
+
+    #[hook(unsafe extern "system" MessageBoxWHook, symbol = "MessageBoxW")]
+    fn MessageBoxWDetour(hwnd: HWND, text: PCWSTR, _caption: PCWSTR, u_type: u32) -> i32 {
+        let caption = w!("Nope, detoured!");
+        unsafe { MessageBoxWHook.call(hwnd, text, caption, u_type) }
+    }
+}
+
 #[no_mangle]
 pub unsafe extern "system" fn DllMain(_module: usize, _reason: u32, _reserved: usize) -> BOOL {
     // DLL_PROCESS_ATTACH
     if _reason == 1 {
+        user32::init_detours().expect("Failed to initialize detours.");
         MessageBoxW(
             HWND(0),
             w!("Hello from isaac-inject.dll!"),
